3e893fa23538f74faded80f5702f84bc80b90732
Yasser <yassercorzo1@gmail.com>
2024-04-18 19:56:28 -0400

    added PCA code

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,6 +6,8 @@ import os
 
 from filtering import *
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 # Path to the original data directory
 original_data_dir = os.path.join(os.getcwd(), 'Geolife Trajectories 1.3/Data')
@@ -56,6 +58,8 @@ df_filtered_china = filter_china_locations(dataset_path)
 ##################################### Automatic Extraction #########################################
 ####################################################################################################
 
+
+########################################### K-means ################################################
 # cluster filtered dataset into 30 clusters (representing the 30 cities in China the datapoints are taken from)
 kmeans = KMeans(n_clusters=30).fit(df_filtered_china[['Longitude', 'Latitude']])
 label = kmeans.labels_
@@ -65,4 +69,32 @@ for i in u_labels:
     plt.scatter(df_filtered_china[label == i , 1] , df_filtered_china[label == i , 2] , label = i)
 plt.legend()
 plt.show()
+
+
+############################################# PCA ##################################################
+std_scaler = StandardScaler()
+# normalize data points so that features have zero mean and unit variance
+scaled_df = std_scaler.fit_transform(df_filtered_china)
+
+# find the optimal number of PCs needed by measuring the explained variance ratio for each pc_num
+# pc_nums that have an explained variance ratio of at least 95% are optimal
+pc_nums = np.arange(14)
+
+var_ratio = []
+for num in pc_nums:
+  pca = PCA(n_components=num)
+  pca.fit(scaled_df)
+  var_ratio.append(np.sum(pca.explained_variance_ratio_))
+
+# Analyzing the Change in Explained Variance Ratio
+plt.figure(figsize=(4,2),dpi=150)
+plt.grid()
+plt.plot(nums,var_ratio,marker='o')
+plt.xlabel('n_components')
+plt.ylabel('Explained variance ratio')
+plt.title('n_components vs. Explained Variance Ratio')
+
+# retrieve optimal pc_num value
+optimal_pc_num = None # to be filled after analyzing graph
+
 

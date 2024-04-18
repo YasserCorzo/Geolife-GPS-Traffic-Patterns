0c769af7e03ab8b21add01709263184696f89843
Eric Yang <ericyang8484@gmail.com>
2024-04-18 13:28:09 -0400

    added filtered preprocessing for labels.txt files

--- filtering.py
+++ filtering.py
@@ -40,10 +40,47 @@ def extract_user_transportation_mode(users_paths, transportation_mode):
             if transportation_mode == 'car' or transportation_mode == 'taxi':
                 if 'car' in line or 'taxi' in line:
                     res.append(user_path)
+                    break
             else:
                 if transportation_mode in line:
                     res.append(user_path)
+                    break
     return res
+
+
+
+def filter_labels_file(label_file_path):
+    """
+    Filter the lines in the labels.txt file to only include those with 'car' or 'taxi' as the transportation mode.
+    
+    Args:
+    - label_file_path (str): Path to the labels.txt file
+    
+    Returns:
+    - filtered_lines (list): List of filtered lines
+    """
+    filtered_lines = []
+    with open(label_file_path, 'r') as file:
+        lines = file.readlines()
+        # Append the header line
+        filtered_lines.append(lines[0])
+        for line in lines[1:]:
+            if 'car' in line or 'taxi' in line:
+                filtered_lines.append(line)
+    return filtered_lines
+
+def create_filtered_labels_file(user_dir, filtered_lines):
+    """
+    Create a new labels_new.txt file in the user directory and write the filtered lines into it.
+    
+    Args:
+    - user_dir (str): Path to the user directory
+    - filtered_lines (list): List of filtered lines from the labels.txt file
+    """
+    new_file_path = os.path.join(user_dir, 'labels_new.txt')
+    with open(new_file_path, 'w') as file:
+        for line in filtered_lines:
+            file.write(line)
 
 # get current directory
 dataset_dir = os.path.join(os.getcwd(), 'Geolife Trajectories 1.3/Data')
@@ -51,8 +88,14 @@ dataset_dir = os.path.join(os.getcwd(), 'Geolife Trajectories 1.3/Data')
 # filter out users with label file
 users_paths = extract_label_users(dataset_dir)
 
-# filter out users with transportation mode of car
+# filter out users with transportation mode of car / taxi
 users_car = extract_user_transportation_mode(users_paths, 'car')
-print(users_car[0])
+print(users_car)
 
 # Process those time ranges and match them up with trajectories
+
+# Assuming users_car contains paths to user directories with car or taxi transportation mode
+for user_dir in users_car:
+    label_file_path = os.path.join(user_dir, 'labels.txt')
+    filtered_lines = filter_labels_file(label_file_path)
+    create_filtered_labels_file(user_dir, filtered_lines)
